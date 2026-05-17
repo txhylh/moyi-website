39617fcf8163393f7dfd359210afbd71ddecffb6
Tangxh <1053217229@qq.com>
2026-05-17 22:48:33 +0800

    add moyi-app for miniprogram web-view

---
+++ moyi-app/assets/index-CfRnNaz6.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CnavRaff.js","assets/index-doTFxjdc.js","assets/index-DzE-YyZh.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-doTFxjdc.js";const _=p("App",{web:()=>r(()=>import("./web-CnavRaff.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
